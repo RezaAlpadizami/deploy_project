903b96c63207e3dee43a59d51068b1251edd66cb
RezaAlpadizami <Alpdizami94@gmail.com>
2021-10-02 22:32:49 +0700

    Fix Unit Test Component

--- src/setupTest.js
+++ src/setupTest.js
@@ -1,27 +1,9 @@
-<<<<<<< HEAD
 import Enzyme from "enzyme";
-=======
-import Enzyme, { configure, shallow, mount } from 'enzyme';
->>>>>>> 610f33eb54901b2490a726933ef5638d57d44b1a
 
 import Adapter from "enzyme-adapter-react-16";
 
-<<<<<<< HEAD
 Enzyme.configure({ adapter: new Adapter() });
-window.matchMedia =
-  window.matchMedia ||
-  function () {
-    return {
-      matches: false,
-      addListener: function () {},
-      removeListener: function () {},
-    };
-  };
+
 module.exports = {
   setupTestFrameworkScriptFile: "./enzyme.setup.js",
 };
-=======
-configure({ adapter: new Adapter() });
-export { shallow, mount};
-export default Enzyme;
->>>>>>> 610f33eb54901b2490a726933ef5638d57d44b1a
